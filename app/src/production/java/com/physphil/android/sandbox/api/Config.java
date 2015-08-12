86955482451fe8ddccf8bf64e8fd9bc608a9408d
physphil <physphil@gmail.com>
2015-08-12 16:49:37 -0400

    added dev/staging/prod

---
+++ app/src/production/java/com/physphil/android/sandbox/api/Config.java
@@ -0,0 +1,11 @@
+package com.physphil.android.sandbox.api;
+
+/**
+ * Configuration constants
+ * Created by Phizz on 15-08-12.
+ */
+public class Config {
+
+    public static final String API_URL = "http://www.baseball.com/API";
+
+}
